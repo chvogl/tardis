26163a0858671b10c0184ce88f616e03b46e7dfd
Christian Vogl <cvogl@mpa-garching.mpg.de>
2015-06-01 11:52:18 +0200

    Implementation of some functionality of the `AtomicDataLeg` class.

--- tardis/atomic/base.py
+++ tardis/atomic/base.py
@@ -3,6 +3,7 @@ import pandas as pd
 from astropy import units as u
 from collections import OrderedDict
 import numpy as np
+import h5py
 
 class AtomicData(object):
     # all_data_types = BaseAtomicDataType.__subclasses__()
@@ -30,11 +31,46 @@ class AtomicData(object):
 
 
 class AtomicDataLeg(AtomicData):
+    """
+ attributes of `AtomData`   |  implementation status
+ _____________________________________________________
+ '_levels'                  |  implementation started (at the moment still problems with indexing in new `AtomicData`)
+ 'has_synpp_refs'           |  not implemented (preliminary implementation similar to has_macro_atom possible)
+ 'symbol2atomic_number'     |  implemented
+ 'has_ion_cx_data'          |  not implemented
+ 'ionization_data'          |  implemented
+ 'atomic_number2symbol'     |  implemented
+ 'macro_atom_references_all'|  not implemented
+ 'synpp_refs'               |  not implemented
+ 'data_sources'             |  ? not needed
+ 'macro_atom_data_all'      |  not implemented
+ 'version'                  |  not needed
+ 'zeta_data'                |  not implemented
+ 'has_zeta_data'            |  not implemented (preliminary implementation similar to has_macro_atom possible)
+ 'uuid1'                    |  not needed
+ 'has_macro_atom'           |  preliminary implementation
+ 'has_collision_data'       |  not implemented
+ 'md5'                      |  not needed
+ '_lines'                   |  not implemented
+ 'atom_data'                |  implemented (however new 'AtomicData' has no masses in `atoms` at the moment)
+
+  Functionality of the `prepare_atom_data` method of the old `AtomData` has to be implemented.
+    """
+
     _atom_data = None
     _ionization_data = None
     _atomic_number2symbol = None
     _symbol2atomic_number = None
     __levels = None
+    __lines = None
+    _synpp_refs = None
+    _macro_atom_data_all = None
+    _zeta_data = None
+    _has_macro_atom = None
+    _has_collision_data = None
+    _has_zeta_data = None
+    _has_synpp_refs = None
+    _has_ion_cx_data = None
 
     def __init__(self, hdf_store, data_types=BaseAtomicDataType.__subclasses__()):
         super(AtomicDataLeg, self).__init__(hdf_store, data_types)
@@ -80,8 +116,41 @@ class AtomicDataLeg(AtomicData):
     @property
     def _levels(self):
         if self.__levels is None:
-            pass
+            self.__levels = self.levels.copy()
+            cols = self.__levels.columns.tolist()
+            cols = [cols[i] for i in [0, 3, 4, 1, 2]]
+            self.__levels = self.__levels[cols]
+            self.__levels['energy'] *= u.eV.to(u.erg)
         return self.__levels
+
+    @property
+    def _lines(self):
+        if self.__lines is None:
+            pass
+        return self.__lines
+
+    @property
+    def md5(self):
+        raise NotImplementedError
+
+    @property
+    def uuid1(self):
+        raise NotImplementedError
+
+    @property
+    def version(self):
+        raise NotImplementedError
+
+    @property
+    def has_macro_atom(self):
+        """Has to be adjusted depending on the implementation of macro atom data in the new `AtomicData`.
+         Here an `hdf-name` macro_atom is assumed for the `class MacroAtom(BaseAtomicDataType)` in plugins.py"""
+        if self._has_macro_atom is None:
+            if hasattr(self, 'macro_atom'):
+                self._has_macro_atom = True
+            else:
+                self._has_macro_atom = False
+        return self._has_macro_atom
 
     @atom_data.setter
     def atom_data(self, value):
